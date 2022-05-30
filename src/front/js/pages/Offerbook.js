9bb0bcafbee54839752acda6f7e4bd91c00adcac
Adexgold90 <diplomatmujeeb@gmail.com>
2022-05-30 17:57:01 +0000

    fixed typos

--- src/front/js/pages/Offerbook.js
+++ src/front/js/pages/Offerbook.js
@@ -14,34 +14,34 @@ export const Offerbook = () => {
         <div className=" container card">
             <div className="card-body">
                 <form>
-                    <div class="row mb-3">
-                        <label for="bookTitle" class="col-sm-2 col-form-label">Title of the book</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="bookTitle" />
+                    <div className="row mb-3">
+                        <label for="bookTitle" className="col-sm-2 col-form-label">Title of the book</label>
+                        <div className="col-sm-10">
+                            <input type="text" className="form-control" id="bookTitle" />
                         </div>
                     </div>
-                    <div class="row mb-3">
-                        <label for="author" class="col-sm-2 col-form-label">Author</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="author" />
+                    <div className="row mb-3">
+                        <label for="author" className="col-sm-2 col-form-label">Author</label>
+                        <div className="col-sm-10">
+                            <input type="text" className="form-control" id="author" />
                         </div>
                     </div>
-                    <div class="row mb-3">
-                        <label for="editor" class="col-sm-2 col-form-label">Editor</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="editor" />
+                    <div className="row mb-3">
+                        <label for="editor" className="col-sm-2 col-form-label">Editor</label>
+                        <div className="col-sm-10">
+                            <input type="text" className="form-control" id="editor" />
                         </div>
                     </div>
-                    <div class="row mb-3">
-                        <label for="genre" class="col-sm-2 col-form-label">Genre</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="genre" />
+                    <div className="row mb-3">
+                        <label for="genre" className="col-sm-2 col-form-label">Genre</label>
+                        <div className="col-sm-10">
+                            <input type="text" className="form-control" id="genre" />
                         </div>
                     </div>
-                    <div class="row mb-3">
-                        <label for="language" class="col-sm-2 col-form-label">Language</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="language" />
+                    <div className="row mb-3">
+                        <label for="language" className="col-sm-2 col-form-label">Language</label>
+                        <div className="col-sm-10">
+                            <input type="text" className="form-control" id="language" />
                         </div>
                     </div>
                     <div class="mb-3">
