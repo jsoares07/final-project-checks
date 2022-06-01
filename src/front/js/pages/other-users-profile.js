96af2afac7d592fca6307878e7785fe76ba68b9d
Adexgold90 <diplomatmujeeb@gmail.com>
2022-06-01 15:29:41 +0000

    card sections updated with content

--- src/front/js/pages/other-users-profile.js
+++ src/front/js/pages/other-users-profile.js
@@ -13,32 +13,45 @@ export const Other_users_profile = () => {
         <div className="card align-items-center" style={{top: "10px"}} >
             <h3>Personal info</h3>
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+              <img
+                  className="rounded-circle mt-5"
+                  width="150px"
+                  src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
+                />
+                <p>Username: </p>
+                <p>Name: </p>
+                <p>City: </p>
+                <p>Member since: </p>
             </div>
         </div>
         {/* Book history */}
         <div class="row">
-  <div class="col-sm-6">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div>
-  <div class="col-sm-6">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div>
-</div>
+            
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Username books offered</h5>
+                  <ul>
+                    <li>Random book 1</li>
+                    <li>Random book 2</li>
+                    <li>Random book 3</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Username books swapped</h5>
+                  <ul>
+                    <li>Random book 1</li>
+                    <li>Random book 2</li>
+                    <li>Random book 3</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+        </div>
         <Footer />
     </div>
   )
