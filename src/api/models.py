7e4e8ee05902098a69e0e303ebc2e96030f5249f
Heylga <oy.golovina@gmail.com>
2022-06-02 10:58:02 +0000

    working on model

--- src/api/models.py
+++ src/api/models.py
@@ -10,21 +10,24 @@ from sqlalchemy import create_engine
 db = SQLAlchemy()
 
 class User(db.Model):
-    __tablename__ = 'users'
+    # __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    first_name = db.Column(db.String(120), unique=False, nullable=False)
-    last_name = db.Column(db.String(120), unique=False, nullable=False)
-    profile_picture = db.Column(db.Text())
-    date_of_birth = db.Column(db.Date)    
-    mobile_number = db.Column(db.String(120), unique=False, nullable=False)
-    address_line = db.Column(db.String(120), unique=False, nullable=False)
-    postcode = db.Column(db.String(120), unique=False, nullable=False)
-    state = db.Column(db.String(120), unique=False, nullable=False)
-    city = db.Column(db.String(120), unique=False, nullable=False)
-    country = db.Column(db.String(120), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    # first_name = db.Column(db.String(120), unique=False, nullable=False)
+    # last_name = db.Column(db.String(120), unique=False, nullable=False)
+    # profile_picture = db.Column(db.Text())
+    # date_of_birth = db.Column(db.Date)    
+    # mobile_number = db.Column(db.String(120), unique=False, nullable=False)
+    # address_line = db.Column(db.String(120), unique=False, nullable=False)
+    # postcode = db.Column(db.String(120), unique=False, nullable=False)
+    # state = db.Column(db.String(120), unique=False, nullable=False)
+    # city = db.Column(db.String(120), unique=False, nullable=False)
+    # country = db.Column(db.String(120), unique=False, nullable=False)
+
 
     # users = relationship('books')    
 
@@ -35,75 +38,76 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            "first_name": self.first_name,
-            "last_name": self.last_name,
-            "date_of_birth": self.date_of_birth,          
-            "profile_picture": self.profile_picture,
-            "mobile_number": self.mobile_number,
-            "address_line": self.address_line,
-            "postcode": self.postcode,
-            "state": self.state,
-            "city": self.city,
-            "country": self.country,
+            "password": self.password,
+            # "first_name": self.first_name,
+            # "last_name": self.last_name,
+            # "date_of_birth": self.date_of_birth,          
+            # "profile_picture": self.profile_picture,
+            # "mobile_number": self.mobile_number,
+            # "address_line": self.address_line,
+            # "postcode": self.postcode,
+            # "state": self.state,
+            # "city": self.city,
+            # "country": self.country,
             # do not serialize the password, its a security breach
         }
 
-    def login_user(email, password):
-        user = User.query.filter_by(email=email, password=password).first()
-        return user
+#     def login_user(email, password):
+#         user = User.query.filter_by(email=email, password=password).first()
+#         return user
     
-    def login_user(event):
-        user = User(email=email, password=password)
-        db.session.add(user)
+#     def login_user(event):
+#         user = User(email=email, password=password)
+#         db.session.add(user)
      
-   #this function will save the new user in the database
-    def save_user(email, password):
+#    #this function will save the new user in the database
+#     def save_user(email, password):
         
-        #lets save the user in the database
-        db.session.add(user)
-        db.session.commit()
+#         #lets save the user in the database
+#         db.session.add(user)
+#         db.session.commit()
 
-    def signup_user():
+#     def signup_user():
        
-        user = User(email=email, password=password, first_name=first_name, user_name=user_name, city=city)
-        db.session.add(user)
+#         user = User(email=email, password=password, first_name=first_name, user_name=user_name, city=city)
+#         db.session.add(user)
 
-    # def edit_user():
+#     # def edit_user():
        
-    #     user = User(first_name=first_name, last_name=last_name, date_of_birth=date_of_birth, email=email, mobile_number=mobile_number, city=city, country=country, profile_picture=profile_picture)
-    #     db.session.add(user)
+#     #     user = User(first_name=first_name, last_name=last_name, date_of_birth=date_of_birth, email=email, mobile_number=mobile_number, city=city, country=country, profile_picture=profile_picture)
+#     #     db.session.add(user)
      
    
    
-class Book(db.Model):
-    __tablename__ = 'books'
+# class Book(db.Model):
+#     __tablename__ = 'books'
 
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120), unique=True, nullable=False)
-    author = db.Column(db.String(80), unique=False, nullable=False)
-    editor = db.Column(db.String(120), unique=False, nullable=False)
-    genre = db.Column(db.String(120), unique=False, nullable=False)
-    language = db.Column(db.String(120), unique=False, nullable=False)
-    description = db.Column(db.String(120), unique=False, nullable=False)  
-    book_picture = db.Column(db.Text())
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(120), unique=True, nullable=False)
+#     author = db.Column(db.String(80), unique=False, nullable=False)
+#     editor = db.Column(db.String(120), unique=False, nullable=False)
+#     genre = db.Column(db.String(120), unique=False, nullable=False)
+#     language = db.Column(db.String(120), unique=False, nullable=False)
+#     description = db.Column(db.String(120), unique=False, nullable=False)  
+#     book_picture = db.Column(db.Text())
 
-    # books = relationship('users')    
+#     # books = relationship('users')    
 
-    def __repr__(self):
-        return f'<Book {self.email}>'
+#     def __repr__(self):
+#         return f'<Book {self.email}>'
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "title": self.title,
-            "author": self.author,
-            "editor": self.editor,
-            "genre": self.genre,          
-            "language": self.language,
-            "description": self.description,
-            "book_picture": self.book_picture,
-            # do not serialize the password, its a security breach
-        }
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "title": self.title,
+#             "author": self.author,
+#             "editor": self.editor,
+#             "genre": self.genre,          
+#             "language": self.language,
+#             "description": self.description,
+#             "book_picture": self.book_picture,
+#             # do not serialize the password, its a security breach
+#         }
 
 
-# render_er(db.Model, 'diagram.png')
+# # render_er(db.Model, 'diagram.png')
--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token
 
 
 api = Blueprint('api', __name__)
@@ -11,86 +12,99 @@ api = Blueprint('api', __name__)
 # Create a route to authenticate your users and return JWTs. The
 # create_access_token() function is used to actually generate the JWT.
 
-@api.route("/token", methods=["POST"])
-def create_token():
-    email = request.json.get("email", None)
-    password = request.json.get("password", None)
-    if email != "test" or password != "test":
-        return jsonify({"msg": "Bad email or password"}), 401
+# @api.route("/token", methods=["POST"])
+# def create_token():
+#     email = request.json.get("email", None)
+#     password = request.json.get("password", None)
+#     if email != "test" or password != "test":
+#         return jsonify({"msg": "Bad email or password"}), 401
 
-    access_token = create_access_token(identity=email)
-    return jsonify(access_token=access_token) 
+#     access_token = create_access_token(identity=email)
+#     return jsonify(access_token=access_token) 
 
 
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+    email = 'Olga'
+    password = '123'
+    access_token = create_access_token(identity=email)
 
-    return jsonify(response_body), 200
-
-    #save the user (from models.py)
-    our_user.save_user()
-    
-    return jsonify(our_user.serialize())
-
-
-@api.route('/login', methods=['GET', 'POST'])
-def login_user(email, password):
-
-    email = request.json.get("email", None)
-    password = request.json.get("password", None)
-
-    print('email', email, 'password', password)
-
-    if email != "test" or password != "test":
-        return {"error": "Wrong email or password"}, 400
-
-    # access_token = create_access_token(identity=email)
-    # response = {"access_token":access_token}
-    return response.jsonify({"msg": "user created"}), 200
-
-
-
-@api.route('/signup', methods=['GET','POST'])
-def register_user(email, password):
-
-    email = request.json.get("email", None)
-    password = request.json.get("password", None)
-
-    #Now we need to create a list of information which should be passed to the DataBase
-    #We dont need to mention ID here, because its set up automatically
-    our_user = User (
-
+    user = User(
         email = email,
-        password = password,
-        is_active = False,
-
+        password = password
     )
 
-    #save the user (from models.py)
-    our_user.save_user()
-    
-    return jsonify(our_user.serialize())
-
-
-
-@api.route('/book', methods=['POST'])
-def register_book():
+    user.serialize()
 
     response_body = {
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+        "access_token": access_token,
+        "user": user.serialize()
     }
 
     return jsonify(response_body), 200
 
     #save the user (from models.py)
-    our_user.save_book()
+    # our_user.save_user()
     
-    return jsonify(our_book.serialize())
+    # return jsonify(our_user.serialize())
+
+
+# @api.route('/login', methods=['GET', 'POST'])
+# def login_user(email, password):
+
+#     email = request.json.get("email", None)
+#     password = request.json.get("password", None)
+
+#     print('email', email, 'password', password)
+
+#     if email != "test" or password != "test":
+#         return {"error": "Wrong email or password"}, 400
+
+#     # access_token = create_access_token(identity=email)
+#     # response = {"access_token":access_token}
+#     return response.jsonify({"msg": "user created"}), 200
+
+
+
+# @api.route('/signup', methods=['GET','POST'])
+# def register_user(email, password):
+
+#     email = request.json.get("email", None)
+#     password = request.json.get("password", None)
+
+#     #Now we need to create a list of information which should be passed to the DataBase
+#     #We dont need to mention ID here, because its set up automatically
+#     our_user = User (
+
+#         email = email,
+#         password = password,
+#         is_active = False,
+
+#     )
+
+#     #save the user (from models.py)
+#     our_user.save_user()
+    
+#     return jsonify(our_user.serialize())
+
+
+
+# @api.route('/book', methods=['POST'])
+# def register_book():
+
+#     response_body = {
+#         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+#     }
+
+#     return jsonify(response_body), 200
+
+#     #save the user (from models.py)
+#     our_user.save_book()
+    
+#     return jsonify(our_book.serialize())
 
 
 
