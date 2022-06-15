3114a581a5966d4e267049267744144f697afb65
Adexgold90 <diplomatmujeeb@gmail.com>
2022-06-15 01:57:47 +0000

    form styling done

--- src/front/js/pages/Reviewuser.js
+++ src/front/js/pages/Reviewuser.js
@@ -17,7 +17,9 @@ export const Reviewuser = () => {
                 <p>How was your exchange with User?</p>
               </div>
               <Rating />
-              <textarea placeholder="Write a review about your book exchange" style={{height: "100px"}}></textarea>
+              <div className='col align-self-center'>
+              <textarea placeholder="Write a review about your book exchange" style={{height: "100px", width: "400px"}}></textarea>
+              </div>
               <div>
                 <button type="button" className="btn btn-success" style={{width: "100px"}}>Success</button>
               </div>
