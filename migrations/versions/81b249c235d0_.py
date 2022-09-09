43305094f83fef3cb3af0376ca74e9c85a38f405
Heylga <oy.golovina@gmail.com>
2022-09-09 14:55:08 +0000

    trying t odeploy to heroku

--- migrations/versions/81b249c235d0_.py
+++ migrations/versions/81b249c235d0_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: c4802d9cff25
+Revision ID: 81b249c235d0
 Revises: 
-Create Date: 2022-09-08 20:01:38.148759
+Create Date: 2022-09-09 14:40:07.623170
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'c4802d9cff25'
+revision = '81b249c235d0'
 down_revision = None
 branch_labels = None
 depends_on = None
--- secrets.js
+++ secrets.js
@@ -1,2 +1,2 @@
 export const GoogleMapAPIKEY = "" 
-export const URLbase = "https://3001-heylga-finalproject-g4udlh5uu3n.ws-eu64.gitpod.io"
+export const URLbase = "https://3001-heylga-finalproject-pzekafigj08.ws-eu64.gitpod.io"
